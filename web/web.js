e9c81ffefa5723ca9f2e7d23be37a6044eee39c7
Willys96 <willys96@gmail.com>
2014-12-16 21:06:12 -0500

    formulario post

--- web/web.js
+++ web/web.js
@@ -11,7 +11,21 @@ function mostrarFormulario(){
 }
 
 function agregarPost () {
-	var url=
+	var url = $url.val(),
+		titulo = $titulo.val(),//val() captura ek valor -value-
+		$clone = $post.clone();
+
+	$clone.find(".titulo_item a") //Busca y adopta las propiedades del control que se busca
+		.text(titulo)
+		.attr("href",url);
+
+	$clone.hide();
+
+	$list.prepend($clone);//Agrega un control como primer elemento de list
+
+	$clone.fadeIn();//lleva a cabo la animación 
+
+
 	return false;//Evita el refresco de la ventana
 }
 
